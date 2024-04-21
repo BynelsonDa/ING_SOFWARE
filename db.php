6472c6c9b23d601eef55ad6192d7e1b7fa67f66a
Nelson07RCC <nelsonrosas955@gmail.com>
2024-04-20 22:57:08 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conex = mysqli_connect("localhost","root","","pollosfinosayd");
+$conex = mysqli_connect("sql204.infinityfree.com","if0_36399305","bi58tfrRnH","if0_36399305_pollosfinosayd");
 
 
 
